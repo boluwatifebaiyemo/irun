c4dae8796e04fae9f590f8e3b4b06d14b318a146
Boluwatife Baiyemogit <boluwatifebaiy@gmail.com>
2024-03-09 10:10:25 +0100

    progress 09March

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,24 @@
+import Link from "next/link";
+
 export default function Index() {
   return (
     <>
-      <main>
-        <div>
-          <h1>Learning stuff</h1>
+      <main className="h-screen bg-desktop">
+        <div className="h-full w-full hidden lg:flex justify-center items-center bg-desktop">
+          <div className="h-full w-full bg-[#402b3a]/45 flex justify-center items-center">
+            <div className="w-full md:w-[520px] flex flex-col gap-6 px-4 md:px-0">
+              <h1 className="text-4xl lg:text-6xl text-center text-[#f8f4ec]">Shop for hairs and book an appointment for a fix</h1>
+              <Link href='/auth/signup' className="h-[48px] flex justify-center items-center bg-[#402b3a] text-[#f8f4ec] text-2xl px-4 rounded-md">Get Started</Link>
+            </div>
+          </div>
+        </div>
+        <div className="h-full w-full  lg:hidden bg-mobile">
+
         </div>
 
-        <p>lorem akfefjhvjfhvejhf hvfhewbjhvefvhwjh fvjhewvfjhevjhfv  fwvefjhvh hjfd</p>
       </main>
     </>
   );
 }
+
+
