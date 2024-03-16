2221f7b1cc5ee615d02d7b56cce003810251cfec
Boluwatife Baiyemogit <boluwatifebaiy@gmail.com>
2024-03-17 00:18:36 +0100

    progress 16-03

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
+
 import Link from "next/link";
 import Head from "next/head";
-import { Merriweather } from "next/font/google"
+import Image from "next/image";
+import { Merriweather } from "next/font/google";
+import { FaArrowRight } from "react-icons/fa6";
+import { Card, CardActions, CardActionArea, CardContent, CardMedia, Typography, Button } from "@mui/material";
 
-const MerriReg400 = Merriweather({
-  subsets:['latin'],
-  display: 'swap',
-  weight: '400'
-});
+
 
 const MerriReg900 = Merriweather({
-  subsets:['latin'],
+  subsets: ['latin'],
   display: 'swap',
   weight: '900'
 });
@@ -18,23 +18,86 @@ const MerriReg900 = Merriweather({
 export default function Index() {
   return (
     <>
-    <Head>
-      <title>Irun | Shop for hairs with fixes</title>
-    </Head>
+      <Head>
+        <title>Irun | Shop for hairs with fixes</title>
+      </Head>
       <main className="h-screen bg-desktop">
         <div className="h-full w-full hidden lg:flex justify-center items-center bg-desktop">
           <div className="h-full w-full bg-[#402b3a]/45 flex justify-center items-center">
-            <div className="w-full md:w-[520px] flex flex-col gap-6 px-4 md:px-0">
+            <div className="w-full md:w-[720px] flex flex-col gap-6 px-4 md:px-0">
               <h1 className={`${MerriReg900.className} text-4xl lg:text-6xl text-center text-[#f8f4ec]`}>Shop for hairs and book an appointment for a fix</h1>
-              <Link href='/auth/signup' className="py-4 flex justify-center items-center bg-[#402b3a] text-[#f8f4ec] text-2xl px-14 mx-auto rounded-md">Get Started</Link>
+              <Link
+                href='/auth/signup'
+                className="py-4 flex justify-center items-center bg-[#402b3a] text-[#f8f4ec] text-2xl px-14 mx-auto rounded-md">
+                <span>Get Started
+                </span>
+                <FaArrowRight />
+              </Link>
             </div>
           </div>
         </div>
-        <div className="h-full w-full  lg:hidden bg-mobile">
 
+        <div className="h-full w-full lg:hidden justify-center items-center bg-mobile">
+          <div className="h-full w-full bg-[#402b3a]/45 flex justify-center items-center">
+            <div className="w-full md:w-[520px] flex flex-col gap-6 px-4 md:px-0">
+              <h1 className={`${MerriReg900.className} text-4xl lg:text-6xl text-center text-[#f8f4ec]`}>Shop for hairs and book an appointment for a fix</h1>
+              <Link
+                href='/auth/signup'
+                className="py-4 flex justify-center items-center bg-[#402b3a] text-[#f8f4ec] text-2xl px-14 mx-auto rounded-md">
+                <span>Get Started
+                </span>
+                <FaArrowRight />
+              </Link>
+            </div>
+          </div>
         </div>
 
-      </main>
+        {/* shop intro section */}
+
+        {/* <section className="grid grid-cols-3">
+          <article>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image="/hair_1.jpg"
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Lizard
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Share
+                </Button>
+              </CardActions>
+            </Card>
+          </article>
+          <aside className="col-span-2">
+            <div className="grid grid-cols-2 gap-2">
+              <Image width={300} height={400} src='/hair_hori.jpg' />
+              <div className="grid grid-cols-2 gap-2">
+                <Image width={200} height={400} src='/hair_1.jpg' alt="hair sample"/>
+                <Image width={200} height={400} src='/hair_2.jpg' alt="hair sample"/>
+                <Image width={200} height={400} src='/hair_3.jpg' alt="hair sample"/>
+                <Image width={200} height={400} src='/hair_4.jpg' alt="hair sample"/>
+              </div>
+            </div>
+          </aside>
+
+        </section> */}
+
+
+
+      </main >
     </>
   );
 }
