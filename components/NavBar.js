7d7792c09f1672f087cde299cc698f57085cd4a1
Boluwatife Baiyemogit <boluwatifebaiy@gmail.com>
2024-03-19 11:39:58 +0100

    progress nav completed

--- components/NavBar.js
+++ components/NavBar.js
@@ -21,56 +21,98 @@ const MerriReg400 = Merriweather({
 
 export function NavBar() {
     const [showMenu, setShowMenu] = React.useState(true);
+    // const [hideMenu, setHideMenu] = React.useState(false);
+
+    // const menuShow = () => {
+    //     setShowMenu(true);
+    //     setHideMenu(true);
+    // }
+    // const menuClose = () => {
+    //     setShowMenu(false);
+    //     setHideMenu(false);
+    // }
 
     return (
-        <nav className="h-[60px] grid grid-cols-2 lg:grid-cols-3 bg-[#402B3A] border-b border-amber-600 px-2 md:px-8 lg:px-16">
+        <>
+            <nav className="h-[60px] grid grid-cols-2 lg:grid-cols-3 bg-[#402B3A] border-b border-amber-600 px-2 md:px-8 lg:px-16">
 
-            {/* show only on desktop */}
-            <ul className="hidden lg:flex flex-row gap-4 items-center">
-                <li><Link className={styles.navLink} href="#">Shop</Link></li>
-                <li><Link className={styles.navLink} href="#">Accessories</Link></li>
-                <li><Link className={styles.navLink} href="#">Showroom</Link></li>
-                <li><Link className={styles.navLink} href="/contact-us">Contact Us</Link></li>
-            </ul>
-
-            {/* show on all devices */}
-            <div className="flex flex-row gap-1 justify-start lg:justify-center items-center ">
-                {/* <p className={`${MerriReg400.className} text-4xl text-gray-100`}>Irun<span>.</span></p> */}
-                <h1 className={`${MerriReg400.className} text-4xl text-gray-100`}>Irun</h1>
-                <GoDotFill className="text-2xl text-amber-400" />
-            </div>
-
-            {/* show on mobile and tablet */}
-            <div className="lg:hidden flex justify-end items-center">
-                <HiMenuAlt3 className={`${showMenu ? "flex" : "hidden"} text-gray-200 text-4xl`} />
-                <MdClose className=" hidden text-gray-200 text-4xl" />
-            </div>
-
-
-
-            {/* show only on desktop */}
-            <div className="hidden lg:flex flex-row gap-6 justify-end">
-                <div className="flex flex-row gap-4">
-                    <blockquote className="flex flex-row items-center">
-                        <FaGlobeAmericas className="text-amber-400" />
-                        <span className="text-gray-200">English</span>
-                        <MdKeyboardArrowDown className="text-gray-200" />
-                    </blockquote>
-                    <blockquote className="flex flex-row items-center">
-                        <BsCurrencyDollar className="text-amber-400" />
-                        <span className="text-gray-200">USD</span>
-                        <MdKeyboardArrowDown className="text-gray-200" />
-                    </blockquote>
-                </div>
-                <ul className="flex flex-row gap-4 items-center ">
-                    <li><Link href="#"><CiSearch className={styles.navLinkIcon} /></Link></li>
-                    <li><Link href="#"><CiUser className={styles.navLinkIcon} /></Link></li>
-                    <li><Link href="#"><CiHeart className={styles.navLinkIcon} /></Link></li>
-                    <li><Link href="#"><CiShoppingCart className={styles.navLinkIcon} /></Link></li>
+                {/* show only on desktop */}
+                <ul className="hidden lg:flex flex-row gap-4 items-center">
+                    <li><Link className={styles.navLink} href="#">Shop</Link></li>
+                    <li><Link className={styles.navLink} href="#">Accessories</Link></li>
+                    <li><Link className={styles.navLink} href="#">Showroom</Link></li>
+                    <li><Link className={styles.navLink} href="/contact-us">Contact Us</Link></li>
                 </ul>
 
+                {/* show on all devices */}
+                <div className="flex flex-row gap-1 justify-start lg:justify-center items-center ">
+                    {/* <p className={`${MerriReg400.className} text-4xl text-gray-100`}>Irun<span>.</span></p> */}
+                    <h1 className={`${MerriReg400.className} text-4xl text-gray-100`}>Irun</h1>
+                    <GoDotFill className="text-2xl text-amber-400" />
+                </div>
+
+                {/* show on mobile and tablet */}
+                <div className="lg:hidden flex justify-end items-center">
+                    <HiMenuAlt3 onClick={() => setShowMenu(true)} className={`${!showMenu ? "flex" : "hidden"} text-gray-200 text-4xl`} />
+                    <MdClose onClick={() => setShowMenu(false)} className={`${showMenu ? "flex" : "hidden"} text-gray-200 text-4xl`} />
+                </div>
+
+
+
+                {/* show only on desktop */}
+                <div className="hidden lg:flex flex-row gap-6 justify-end">
+                    <div className="flex flex-row gap-4">
+                        <blockquote className="flex flex-row items-center">
+                            <FaGlobeAmericas className="text-amber-400" />
+                            <span className="text-gray-200">English</span>
+                            <MdKeyboardArrowDown className="text-gray-200" />
+                        </blockquote>
+                        <blockquote className="flex flex-row items-center">
+                            <BsCurrencyDollar className="text-amber-400" />
+                            <span className="text-gray-200">USD</span>
+                            <MdKeyboardArrowDown className="text-gray-200" />
+                        </blockquote>
+                    </div>
+                    <ul className="flex flex-row gap-4 items-center ">
+                        <li><Link href="#"><CiSearch className={styles.navLinkIcon} /></Link></li>
+                        <li><Link href="#"><CiUser className={styles.navLinkIcon} /></Link></li>
+                        <li><Link href="#"><CiHeart className={styles.navLinkIcon} /></Link></li>
+                        <li><Link href="#"><CiShoppingCart className={styles.navLinkIcon} /></Link></li>
+                    </ul>
+
+                </div>
+            </nav>
+
+            <div className={`${showMenu ? "flex" : "hidden"} flex-col gap-6 items-center justify-center lg:hidden min-h-80 w-full absolute top-[60px] left-0 z-10 bg-gray-800`}>
+                <ul className="flex flex-col gap-3">
+                    <li className="text-gray-200 text-2xl text-center"><Link href="#">Shop</Link></li>
+                    <li className="text-gray-200 text-2xl text-center"><Link href="#">Accessories</Link></li>
+                    <li className="text-gray-200 text-2xl text-center"><Link href="#">Showroom</Link></li>
+                    <li className="text-gray-200 text-2xl text-center"><Link href="/contact-us">Contact Us</Link></li>
+                </ul>
+                <div className="flex flex-col gap-3">
+                    <ul className="flex flex-row gap-4 items-center ">
+                        <li><Link href="#"><CiSearch className={styles.navLinkIcon} /></Link></li>
+                        <li><Link href="#"><CiUser className={styles.navLinkIcon} /></Link></li>
+                        <li><Link href="#"><CiHeart className={styles.navLinkIcon} /></Link></li>
+                        <li><Link href="#"><CiShoppingCart className={styles.navLinkIcon} /></Link></li>
+                    </ul>
+
+                    <div className="flex flex-row gap-4">
+                        <blockquote className="flex flex-row items-center">
+                            <FaGlobeAmericas className="text-amber-400" />
+                            <span className="text-gray-200">English</span>
+                            <MdKeyboardArrowDown className="text-gray-200" />
+                        </blockquote>
+                        <blockquote className="flex flex-row items-center">
+                            <BsCurrencyDollar className="text-amber-400" />
+                            <span className="text-gray-200">USD</span>
+                            <MdKeyboardArrowDown className="text-gray-200" />
+                        </blockquote>
+                    </div>
+                </div>
             </div>
-        </nav>
+        </>
     )
 }
 
--- pages/contact-us.js
+++ pages/contact-us.js
@@ -7,7 +7,7 @@ import { FaPhoneAlt } from 'react-icons/fa';
 import { FaInstagram } from 'react-icons/fa';
 import { FaFacebook } from 'react-icons/fa';
 import { FaXTwitter } from 'react-icons/fa6';
-import { themeColors } from "@/utils/theme";
+
 
 const MerriReg400 = Merriweather({
     subsets: ['latin'],
@@ -21,8 +21,8 @@ export default function ContactUs() {
             <Head>
                 <title> Contact Us | Irun</title>
             </Head>
-            <main className=" flex flex-col gap-6 px-4 md:px-16 lg:px-32 py-8 md:py-16">
-                <h1 className={'${MerriReg400.className} text-5xl text-center'}>Contact Us</h1>
+            <main className=" flex flex-col gap-6 px-4 md:px-16 lg:px-32 py-8 md:py-16 h-full">
+                <h1 className={`${MerriReg400.className} text-5xl text-center`}>Contact Us</h1>
                 {/* top section */}
                 <div className="flex flex-col gap-4">
                     <blockquote className="min-h-32 flex flex-row gap-4 justify-center items-center border border-gray-300 rounded-md shadow-md">
@@ -44,19 +44,19 @@ export default function ContactUs() {
                 </div>
                 {/* {lower division}  */}
                 <div className="flex flex-col gap-4">
-                    <h2 className={'${MerriReg400.className} text-3xl text-center'}>Engage With Us Across Our Platforms</h2>
+                    <h2 className={`${MerriReg400.className} text-3xl text-center`}>Engage With Us Across Our Platforms</h2>
 
                     <div className="grid md:grid-cols-3 gap-4">
                         <blockquote className="min-h-32 flex flex-row gap-4 justify-center items-center border border-gray-300 rounded-md shadow-md">
-                            <FaInstagram className={⁠ text-[${themeColors.strongPink}] ⁠}/>
+                            <FaInstagram className="text-[#d63484] text-4xl" />
                             <Link href='http://instagram.com/early_code_tech' className=''>@early_code_tech</Link>
                         </blockquote>
                         <blockquote className="min-h-32 flex flex-row gap-4 justify-center items-center border border-gray-300 rounded-md shadow-md">
-                            <FaFacebook className={⁠ text-[${themeColors.strongPink}] ⁠}/>
+                            <FaInstagram className="text-[#d63484] text-4xl" />
                             <Link href='http://facebook.com/earlycodetech' className=''>@early_code_tech</Link>
                         </blockquote>
                         <blockquote className="min-h-32 flex flex-row gap-4 justify-center items-center border border-gray-300 rounded-md shadow-md">
-                            <FaXTwitter className={⁠ text-[${themeColors.strongPink}] ⁠}/>
+                            <FaInstagram className="text-[#d63484] text-4xl" />
                             <Link href='http://twitter.com/earlycodetech' className=''>@early_code_tech</Link>
                         </blockquote>
                     </div>
